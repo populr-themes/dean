2ad04d2e4dca6ee654d1689d4f9020cdd90581ba
morleyb <morley@populr.me>
2013-10-29 23:20:19 -0500

    mobile fixes including center logo and title, plus padding adjustments and removal of image hover effect

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).on('pop-initialized', function(){
   $('.asset-type-imagegroup').live('initialize', function(){
     var ss = $(this).find('.populr-image-slider .slideshow');
-    if (ss.find('.overlay').length == 0) {
+    var ss_next = ss.next();
+    if (ss.find('.overlay').length === 0 && !ss_next.hasClass('overlay')) {
       ss.after('<div class="overlay">&nbsp;</div>');
     }
   });
