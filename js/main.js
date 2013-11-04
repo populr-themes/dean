8cbe8212f03e84b98a39ae2680f1592fba46fd14
morleyb <morley@populr.me>
2013-11-04 13:53:40 -0600

    sync repository to remote

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).on('pop-initialized', function(){
   $('.asset-type-imagegroup').live('initialize', function(){
     var ss = $(this).find('.populr-image-slider .slideshow');
-    var ss_next = ss.next();
-    if (ss.find('.overlay').length === 0 && !ss_next.hasClass('overlay')) {
+    if (ss.find('.overlay').length == 0) {
       ss.after('<div class="overlay">&nbsp;</div>');
     }
   });
